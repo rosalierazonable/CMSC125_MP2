4d23c40d693abd9e3bf29e85395ab41ebc50c8eb
rosalierazonable <rsrazonable1@up.edu.ph>
2021-05-19 12:24:20 +0800

    add FCFS class for FCFS implementation

---
+++ src/com/rosalieraz/cmsc125/FCFS.java
@@ -0,0 +1,19 @@
+package com.rosalieraz.cmsc125;
+
+public class FCFS {
+    int calculateWaitingTime() {
+        return 0;
+    }
+
+    int calculateAvgWaitingTime() {
+     return 0;
+    }
+
+    int calculateTurnaroundTime() {
+        return 0;
+    }
+
+    int calculateAVgTurnaroundTime() {
+        return 0;
+    }
+}
